14342200b80810f8c93498cede52543394cb8342
Juan Pablo Monsalve <juaco7720@gmail.com>
2026-01-25 20:44:56 +0100

    add node module

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/web_project_around_react/', // 🔹 para GitHub Pages
   server: {
-    port: 3000,
+    port: 3000, // 🔹 si quieres tu puerto local
   },
 });
