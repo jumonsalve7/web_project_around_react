d39c21447397b071229e684934ce9e41472f5933
Juan Pablo Monsalve <juaco7720@gmail.com>
2026-01-25 21:02:24 +0100

    my commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/web_project_around_react/', // 🔹 para GitHub Pages
+  base: "/web_project_around_react/", 
   server: {
-    port: 3000, // 🔹 si quieres tu puerto local
-  },
+    port: 3000 
+  }
 });
